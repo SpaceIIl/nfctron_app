102405ea7b101adf1bd3ab35f364c3856d26297b
SpaceIIl <hrujan@outlook.com>
2023-05-02 21:02:41 +0200

    master

--- app/src/main/java/com/example/nfctron_app/spacexLaunches/launches/LaunchesFragment.kt
+++ app/src/main/java/com/example/nfctron_app/spacexLaunches/launches/LaunchesFragment.kt
@@ -1,19 +1,31 @@
 package com.example.nfctron_app.spacexLaunches.launches
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.nfctron_app.R
 import com.example.nfctron_app.databinding.FragmentLaunchesBinding
 import com.example.nfctron_app.databinding.ItemLaunchBinding
+import com.example.nfctron_app.nasaDaily.databaseNasaDaily.NasaDailyRepository
+import com.example.nfctron_app.spacexLaunches.modelLaunches.LaunchesItem
+import kotlinx.coroutines.launch
 
 class LaunchesFragment : Fragment() {
     private var _binding: FragmentLaunchesBinding? = null
@@ -35,6 +47,8 @@ class LaunchesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
 
         binding.textPinned.text = getString(R.string.pinned)
         binding.textUnpinAll.text = getString(R.string.unpin_all)
@@ -72,6 +86,30 @@ class LaunchesFragment : Fragment() {
                         }
                     }
 
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        val composeCarousel = view.findViewById<ComposeView>(R.id.composeCarousel)
+
+                        composeCarousel.setContent {
+                            val carouselItems = state.data
+                            val columnWidthScreen = availableWidthDp().toInt()
+                            CarouselView(items = carouselItems, columns = 2, columnWidth = columnWidthScreen) { item, columnWidth ->
+                                val binding = ItemLaunchBinding.inflate(LayoutInflater.from(context), null, false)
+
+                                binding.textLaunchTitle.text = item.name
+                                binding.textCountdown.text = item.date_local.toString()
+                                if (item.links?.patch?.small != null) {
+                                    binding.imageBadge.load(item.links.patch.small)
+                                } else {
+                                    binding.imageBadge.load(R.drawable.baseline_rocket_launch_24)
+                                }
+                                binding.textLivestream.text = binding.root.context.getString(R.string.livestream)
+                                binding.textWiki.text = binding.root.context.getString(R.string.wiki)
+
+                                AndroidView(factory = { binding.root }, modifier = Modifier.width(columnWidth.dp))
+                            }
+                        }
+                    }
+
                     launchesAdapter.submitList(state.data)
                     binding.recyclerPinned.post {
                         binding.recyclerPinned.layoutManager?.scrollToPosition(0)
@@ -83,12 +121,38 @@ class LaunchesFragment : Fragment() {
 }
 
 @Composable
-fun CarouselItem() {
-    AndroidView(
-        factory = { context ->
-            val inflater = LayoutInflater.from(context)
-            val binding = ItemLaunchBinding.inflate(inflater, null, false)
-            binding.root
-        },
-    )
+fun CarouselView(
+    items: List<LaunchesItem>,
+    columns: Int,
+    columnWidth: Int,
+    content: @Composable (LaunchesItem, Int) -> Unit
+) {
+    LazyHorizontalGrid(
+        GridCells.Fixed(columns),
+        contentPadding = PaddingValues(start = 48.dp)
+    ) {
+        items(count = items.size) { index ->
+            val item = items[index]
+            Row {
+                Box(modifier = Modifier.width(columnWidth.dp)) {
+                    content(item, columnWidth)
+                }
+
+                Spacer(modifier = Modifier.width(48.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun availableWidthDp(): Float {
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp.toFloat()
+
+    val density = LocalDensity.current
+    val twelveDpInPixels = with(density) { 28.dp.toPx() }
+
+    val availableWidthMinus12dp = screenWidthDp - twelveDpInPixels
+
+    return availableWidthMinus12dp
 }
